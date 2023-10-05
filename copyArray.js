0efdc957bc8e118ff4347113e3890126769804ee
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-10-05 23:35:52 +0530

    array values copying

---
+++ copyArray.js
@@ -0,0 +1,12 @@
+const array =[1,2,3,4,5,6,7,8,9,10]
+
+let copy = [],
+k=0;
+for(let i = 0 ; i < array.length; i++){
+    if(array[i] % 2 == 0){
+        copy[k] = array[i];
+        k++;
+    }
+}
+
+console.log(copy);
